282c92ae3b56d0c40a6afb684b233e3b7a14506d
alya1b <93484747+alya1b@users.noreply.github.com>
2023-10-13 16:44:24 +0300

    Update label_control.py

--- src/Program/label_control.py
+++ src/Program/label_control.py
@@ -1,5 +1,6 @@
 from PIL import ImageTk 
 import customtkinter as ck 
+from datetime import timedelta
 
 labels = {}
 
@@ -15,3 +16,11 @@ def set_image(name: str, imgtk: ImageTk.PhotoImage):
   labels[name].imgtk = imgtk 
   labels[name].configure(image=imgtk)
   labels[name].update()
+
+def update_timer_label(name: str, start_time = None, elapsed_time=None, total_duration = None):
+    if elapsed_time is not None:
+        elapsed_time_str = str(timedelta(seconds=elapsed_time))
+        total_duration_str = str(timedelta(seconds=total_duration))
+        labels[name].config(text=f'{elapsed_time_str} / {total_duration_str}')
+    else:
+        labels[name].config(text=' 00:00:00 / 00:00:00')
