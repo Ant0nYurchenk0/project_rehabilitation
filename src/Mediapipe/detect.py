9b84febba666a6511aef94beff12c4a6ba460970
Ant0nYurchenk0 <an.yurchenko.an@gmail.com>
2023-10-26 19:47:11 +0300

    added frames writing to file

--- src/Mediapipe/detect.py
+++ src/Mediapipe/detect.py
@@ -6,6 +6,8 @@ import Mediapipe.mediapipe_helper as mh
 import Program.globalv as gl
 import Writer.writer as writer
 import pathlib
+from datetime import timedelta
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -15,6 +17,7 @@ path = pathlib.Path().resolve()
 
 frame_count = 0
 total_frames = 0
+frames = []
 
 def detect(filename: str=None): 
   filepath = str(path) + "\\output\\"	
@@ -24,8 +27,10 @@ def detect(filename: str=None):
   stored_landmarks = []
   stored_timestamps = []
   stored_angles = []
+
   global frame_count
   global total_frames
+
   total_frames = 0
   frame_count=0
   
@@ -51,6 +56,9 @@ def detect(filename: str=None):
         ('angles', writer.get_angles_csv(stored_angles))
     ], output_path, output_name)
   
+  global frames
+  writer.output_video(frames, output_path, output_name)
+  frames.clear()
 
           
 def process(cap, stored_landmarks: list = [], stored_angles: list = [], stored_timestamps: list = []):
@@ -61,23 +69,46 @@ def process(cap, stored_landmarks: list = [], stored_angles: list = [], stored_t
     if image is None: 
         cap.release()	
         return 
+    image = cv2.flip(image,1)
     
     # Get the current timestamp
     timestamp = time.time()
-    #gl.elapsed_time = timestamp - gl.start_time
     global frame_count
     global total_frames
     frame_count += 1
 
     # Calculate the elapsed time as a fraction of total duration based on frame count
-    if gl.total_duration and frame_count > 0:
+    if gl.total_duration and frame_count > 1:
         elapsed_time = gl.total_duration * frame_count / total_frames
         gl.elapsed_time = elapsed_time
+
+        elapsed_time = timedelta(seconds=elapsed_time)
+        elapsed_seconds = elapsed_time.seconds
+        elapsed_microseconds = elapsed_time.microseconds
+
+        total_time = timedelta(seconds=gl.total_duration)
+        total_seconds = total_time.seconds
+        total_microseconds = total_time.microseconds
+
+        timestamp_str = f"{elapsed_seconds}:{str(elapsed_microseconds)[:2]} / {total_seconds}:{str(total_microseconds)[:2]}"
+
+        image = cv2.putText(image, 
+                            text = timestamp_str,
+                            org = (50, 50), 
+                            fontFace = cv2.FONT_HERSHEY_SIMPLEX,  
+                            fontScale = 1, 
+                            color = (255, 0, 0), 
+                            thickness = 2, 
+                            lineType = cv2.LINE_AA)
     else:
         gl.elapsed_time = None
+  
+    
 
     mh.show_frame(image)
-    mh.update_timers(gl.start_time, gl.elapsed_time, gl.total_duration)
+    global frames
+    frames.append(image)
+
     landmarks = results.pose_landmarks
     if landmarks is not None:
         # Add the timestamp to the stored timestamps
@@ -86,6 +117,5 @@ def process(cap, stored_landmarks: list = [], stored_angles: list = [], stored_t
         stored_landmarks.append(landmarks.landmark)
         angles = mh.get_angles(landmarks.landmark)
         stored_angles.append(angles)
-        # mh.show_angles(angles)
 
 
--- src/Program/program.py
+++ src/Program/program.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import Tkinter.tkinter_helper as th
 import Tkinter.tkinter_functions as tf
 import Program.label_control as lc
-import Program.globalv as gl
 def main():
   root = tk.Tk()
   root.title('GymBro - testing')
@@ -15,27 +14,6 @@ def main():
   th.add_command_to_menu(window_menu, "Exit", root.quit)
 
   #========creating frames=========
-  
-  # angles_frame = th.add_frame(root, 0,400,"green", 300, 700)
-
-  # th.add_label(angles_frame, 0, 10, 'Лівий лікоть')
-  # lc.add_label("left_elbow",  th.add_label(angles_frame, 121, 10, '0'))
-
-  # th.add_label(angles_frame, 0, 51, 'Ліве плече')
-  # lc.add_label("left_shoulder", th.add_label(angles_frame, 121, 51, '0'))
-
-  # th.add_label(angles_frame, 0, 92, 'Ліве коліно')
-  # lc.add_label("left_knee", th.add_label(angles_frame, 121, 92, '0'))
-
-  # th.add_label(angles_frame, 350, 10, 'Правий лікоть')
-  # lc.add_label("right_elbow", th.add_label(angles_frame, 500, 10, '0'))
-
-  # th.add_label(angles_frame, 350, 51, 'Праве плече')
-  # lc.add_label("right_shoulder", th.add_label(angles_frame, 500, 51, '0'))
-
-  # th.add_label(angles_frame, 350, 92, 'Праве коліно')
-  # lc.add_label("right_knee", th.add_label(angles_frame, 500, 92, '0'))
-
 
   mp_live_frame = th.add_frame(root, 0,0,"blue", 400, 700)
   lc.add_label("image",  th.add_label(mp_live_frame, 0, 0, ''))
@@ -45,9 +23,6 @@ def main():
   th.add_command_to_menu(mediapipe_menu, "Live Video", lambda: tf.start_mediapipe_live_video())
   th.add_command_to_menu(mediapipe_menu, "From file", lambda: tf.start_mediapipe_from_file())
 
-  timer_label = tk.Label(root, text='00:00:00 / 00:00:00')
-  timer_label.pack(anchor='ne')
-  lc.add_label("timer", timer_label)
 
   root.mainloop()
 
